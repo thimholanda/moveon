c2f350aedd4982c5af2a092c4c84934e2cbf7982
Thiago Holanda <thimholanda@gmail.com>
2023-07-04 19:52:18 -0300

    refactor(evaluationsModal): remove form block for empty values

--- src/components/Modal/EvaluationModal.tsx
+++ src/components/Modal/EvaluationModal.tsx
@@ -33,8 +33,6 @@ export function EvaluationModal() {
   const [weight, setWeight] = useState("");
 
   function handleClose() {
-    if (!squats || !abs || !weight) return;
-
     const evaluation: Evaluation = {
       squats,
       abs,
@@ -82,45 +80,17 @@ export function EvaluationModal() {
       <Flex p={5} direction={"column"} alignItems={"center"}>
         <Text color={"blue.500"} fontSize={"2xl"} textAlign={"center"}>
           É a hora de registrar sua evolução!
-          <Button
-            border={"1px"}
-            mb={5}
-            mt={1}
-            bg={"transparent"}
-            onClick={onOpen}
-            _hover={{ color: "white" }}
-          >
-            Clique aqui e veja como executar{" "}
-            <Icon
-              fontSize={"2xl"}
-              cursor={"pointer"}
-              as={RiQuestionFill}
-              ms={2}
-            ></Icon>
+          <Button border={"1px"} mb={5} mt={1} bg={"transparent"} onClick={onOpen} _hover={{ color: "white" }}>
+            Clique aqui e veja como executar <Icon fontSize={"2xl"} cursor={"pointer"} as={RiQuestionFill} ms={2}></Icon>
           </Button>
         </Text>
         <Text mb={6} fontSize={"lg"} color={"gray.200"} textAlign={"center"}>
-          Marque o número máximo de repetições que conseguir fazer em 1 minuto
-          de cada exercício.
+          Marque o número máximo de repetições que conseguir fazer em 1 minuto de cada exercício.
         </Text>
         <Flex as="form" width="100%" borderRadius={8} flexDir="column">
           <SimpleGrid columns={isWideVersion ? 3 : 1} spacing={4} mb={6}>
-            <Input
-              isTransparent
-              name="squats"
-              type="text"
-              label="Agachamentos"
-              onChange={(e) => setSquats(e.target.value)}
-              isRequired
-            />
-            <Input
-              isTransparent
-              name="abs"
-              type="text"
-              label="Abdominais"
-              onChange={(e) => setAbs(e.target.value)}
-              isRequired
-            />
+            <Input isTransparent name="squats" type="text" label="Agachamentos" onChange={(e) => setSquats(e.target.value)} isRequired />
+            <Input isTransparent name="abs" type="text" label="Abdominais" onChange={(e) => setAbs(e.target.value)} isRequired />
             {/* <Input
               isTransparent
               name="genre"
@@ -128,86 +98,29 @@ export function EvaluationModal() {
               label="Flexões"
               onChange={(e) => setPushUps(e.target.value)}
             /> */}
-            <Input
-              isTransparent
-              name="weight"
-              type="text"
-              label="Peso atual"
-              onChange={(e) => setWeight(e.target.value)}
-              isRequired
-            />
+            <Input isTransparent name="weight" type="text" label="Peso atual" onChange={(e) => setWeight(e.target.value)} isRequired />
           </SimpleGrid>
         </Flex>
       </Flex>
       <Flex py={10} pt={4} alignItems={"center"} justifyContent={"center"}>
-        <Button
-          py="5"
-          cursor="pointer"
-          size="sm"
-          fontSize="lg"
-          colorScheme="pink"
-          rightIcon={<Icon as={RiCheckLine} fontSize="20" />}
-          onClick={handleClose}
-        >
+        <Button py="5" cursor="pointer" size="sm" fontSize="lg" colorScheme="pink" rightIcon={<Icon as={RiCheckLine} fontSize="20" />} onClick={handleClose}>
           Enviar
         </Button>
       </Flex>
 
-      <Modal
-        onClose={onCloseLocal}
-        isOpen={isOpen}
-        size={"xl"}
-        scrollBehavior="inside"
-      >
+      <Modal onClose={onCloseLocal} isOpen={isOpen} size={"xl"} scrollBehavior="inside">
         <ModalOverlay bgColor={"rgba(6, 6, 7, .96)"} />
-        <ModalContent
-          borderRadius={10}
-          m={5}
-          border={"1px solid white"}
-          borderColor={"gray.400"}
-          bg={"rgba(179, 181, 198, 0.1)"}
-        >
+        <ModalContent borderRadius={10} m={5} border={"1px solid white"} borderColor={"gray.400"} bg={"rgba(179, 181, 198, 0.1)"}>
           <Flex p={5} direction={"column"} alignItems={"center"}>
-            <Text
-              mb={6}
-              fontSize={"lg"}
-              color={"gray.200"}
-              textAlign={"center"}
-            >
+            <Text mb={6} fontSize={"lg"} color={"gray.200"} textAlign={"center"}>
               Veja como fazer os exercícios da avaliação física.
             </Text>
-            <Box
-              as={"div"}
-              mb={"4"}
-              w={"100%"}
-              borderRadius={"8"}
-              overflow={"hidden"}
-            >
-              <Vimeo
-                responsive
-                video={631339894}
-                showTitle={false}
-                showByline={false}
-                showPortrait={false}
-              />
+            <Box as={"div"} mb={"4"} w={"100%"} borderRadius={"8"} overflow={"hidden"}>
+              <Vimeo responsive video={631339894} showTitle={false} showByline={false} showPortrait={false} />
             </Box>
           </Flex>
-          <Flex
-            borderTop={"1px solid white"}
-            borderColor={"gray.400"}
-            py={10}
-            alignItems={"center"}
-            justifyContent={"center"}
-          >
-            <Button
-              py="5"
-              cursor="pointer"
-              size="sm"
-              fontSize="lg"
-              colorScheme="pink"
-              rightIcon={<Icon as={RiArrowRightLine} fontSize="20" />}
-              onClick={onCloseLocal}
-            >
+          <Flex borderTop={"1px solid white"} borderColor={"gray.400"} py={10} alignItems={"center"} justifyContent={"center"}>
+            <Button py="5" cursor="pointer" size="sm" fontSize="lg" colorScheme="pink" rightIcon={<Icon as={RiArrowRightLine} fontSize="20" />} onClick={onCloseLocal}>
               Continuar
             </Button>
           </Flex>
